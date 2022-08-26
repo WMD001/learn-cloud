45fbe5030d3a4acee2a722745ffa77ae085fbb9c
Qiq <habitdev@163.com>
2022-08-27 00:40:42 +0800

    修改忽略配置，添加eureka模块，删除parent中会自动加载但是用不到的数据库连接依赖

--- learn-cloud-eureka/src/main/java/com/cloud/LearnCloudEurekaApplication.java
+++ learn-cloud-eureka/src/main/java/com/cloud/LearnCloudEurekaApplication.java
@@ -5,24 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
-/**
- * @author Qiq
- */
-@EnableEurekaClient
-@ServletComponentScan
+@EnableEurekaServer
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         MongoDataAutoConfiguration.class,
         MongoAutoConfiguration.class,
 })
-public class LearnCloudWebApplication {
+public class LearnCloudEurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearnCloudWebApplication.class, args);
+        SpringApplication.run(LearnCloudEurekaApplication.class, args);
     }
 
 }
---
+++ learn-cloud-eureka/src/test/java/com/cloud/learncloudeureka/LearnCloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.learncloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnCloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
