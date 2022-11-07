1be51c4b8e587a5d8d36d74aee1235dcb3cf311f
Qiq <habitdev@163.com>
2022-11-07 23:16:31 +0800

    添加data空模块，完成数据增删改查功能

---
+++ learn-cloud-data/src/main/java/com/cloud/LearnCloudDataApplication.java
@@ -0,0 +1,13 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCloudDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnCloudDataApplication.class, args);
+    }
+
+}
